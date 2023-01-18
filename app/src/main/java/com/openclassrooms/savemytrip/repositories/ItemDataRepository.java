018793b0ab87dda82f70a8b50e0c41cb1844e8de
ManonM <105633434+mmenichon@users.noreply.github.com>
2023-01-18 09:49:54 +0100

    Création des repositories

---
+++ app/src/main/java/com/openclassrooms/savemytrip/repositories/ItemDataRepository.java
@@ -0,0 +1,38 @@
+package com.openclassrooms.savemytrip.repositories;
+
+import androidx.lifecycle.LiveData;
+
+import com.openclassrooms.savemytrip.database.dao.ItemDao;
+import com.openclassrooms.savemytrip.models.Item;
+
+import java.util.List;
+
+public class ItemDataRepository {
+
+    private final ItemDao itemDao;
+
+    public ItemDataRepository(ItemDao itemDao) {
+        this.itemDao = itemDao;
+    }
+
+    // GET
+    public LiveData<List<Item>> getItems(long userId) {
+        return this.itemDao.getItems(userId);
+    }
+
+    // CREATE
+    public void createItem(Item item) {
+        itemDao.insertItem(item);
+    }
+
+    // DELETE
+    public void deleteItem(long itemId) {
+        itemDao.deleteItem(itemId);
+    }
+
+    // UPDATE
+    public void updateItem(Item item) {
+        itemDao.updateItem(item);
+    }
+
+}
--- app/src/main/java/com/openclassrooms/savemytrip/repositories/UserDataRepository.java
+++ app/src/main/java/com/openclassrooms/savemytrip/repositories/UserDataRepository.java
@@ -1,5 +1,20 @@
 package com.openclassrooms.savemytrip.repositories;
 
+import androidx.lifecycle.LiveData;
+
+import com.openclassrooms.savemytrip.database.dao.UserDao;
+import com.openclassrooms.savemytrip.models.User;
+
 public class UserDataRepository {
-    
+
+    private final UserDao userDao;
+
+    public UserDataRepository(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public LiveData<User> getUser(long userId) {
+        return this.userDao.getUser(userId);
+    }
+
 }
